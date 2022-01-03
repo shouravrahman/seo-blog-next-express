03e600d1867ae4c94e57b10aff7a3118de1b4b79
shouravrahman <shouravrahman006@gmail.com>
2022-01-03 17:59:43 +0600

    font extend

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['ui-sans-serif', 'system-ui'],
+				serif: ['ui-serif', 'Georgia'],
+				mono: ['ui-monospace', 'SFMono-Regular'],
+				lato: ['Lato'],
+			},
+		},
 	},
 	plugins: [],
 }
